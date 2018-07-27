d73cfee5085fb70d91badd407f90123c32afdb0e
Unknown <Isaac.overlander@gmail.com>
2018-07-27 16:00:16 -0500

    Button update
    Button appends info to the DOM and the table displays correct info

--- client.js
+++ client.js
@@ -1,18 +1,34 @@
 $(document).ready(readyNow);
 
-function readyNow() {
-    $('#submit-btn').on('click', submitInfo)
+function readyNow() { 
+    appendDom();  
 }
 
-function submitInfo() {
-    let firstName = $('#firstNameIn').val();
-    console.log(firstName);
-    let lastName = $('#lastNameIn').val();
-    console.log(lastName);
-    let idNumber = $('#idIn').val();
-    console.log(idNumber);
-    let title = $('#titleIn').val();
-    console.log(title);
-    let annualSalary = $('#salaryIn').val();
-    console.log(annualSalary);
+function appendDom() {
+    let table = $('<table></table>');
+    table.append('<thead><tr><th>First Name</th><th>Last Name</th><th>Id Number</th><th>Employee Title</th><th>Annual Salary</th></thead>');
+  
+    let tbody = $('<tbody id="tableBody"></tbody>');
+    table.append(tbody);
+  
+    $('.container').append(table);
+
+    $('#submit-btn').on('click', submitInfo)
+
+    function submitInfo() {
+        //colect info from inputs
+        let firstName = $('#firstNameIn').val();
+        let lastName = $('#lastNameIn').val();
+        let idNumber = $('#idIn').val();
+        let title = $('#titleIn').val();
+        let annualSalary = $('#salaryIn').val();
+    
+        tbody.append('tr><td>' + firstName + '</td><td>' + lastName +'</td><td>' + idNumber + '</td><td>' + title + '</td><td>' + annualSalary + '</td>');
+    
+        $('#firstNameIn').val('');
+        $('#lastNameIn').val('');
+        $('#idIn').val('');
+        $('#titleIn').val('');
+        $('#salaryIn').val('');
+    }
 }
